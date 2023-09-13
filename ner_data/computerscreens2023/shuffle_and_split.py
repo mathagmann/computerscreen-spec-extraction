c179967e10317759105012bbfcb542c8e6d9d2d6
MattHag <matthias.hagmann@gmx.at>
2023-09-13 20:31:55 +0200

    Shuffles and splits .conll into .tsv (train, test, valid)
    Creates .tsv dataset files by converting, shuffling and splitting .conll (with token, label pairs) and writes token, label pairs tab-separated into three .tsv files.
    
    Tests splitting.
    
    Relates #12

---
+++ ner_data/computerscreens2023/shuffle_and_split.py
@@ -0,0 +1,98 @@
+"""Converts the CoNLL file into train, valid, and test .tsv sets.
+
+Creates tab-separated .tsv files with two columns: word, label.
+"""
+import random
+from pathlib import Path
+from typing import Tuple
+
+DELIMITER_CONLL = " -X- _ "
+DELIMITER_TSV = "\t"
+
+COLUMNS = ["tokens", "ner_tags"]
+
+
+def _read_data(file_path):
+    """Reads CoNLL data and returns it as a list of (text, labels) pairs."""
+    inputs, labels = [], []
+    with open(file_path, "r") as f:
+        token_list, label_list = [], []
+        for i, line in enumerate(f):
+            line = line.strip()
+            if line.startswith("-DOCSTART-"):
+                if i == 0:
+                    continue
+                raise ValueError("Only the first line should be -DOCSTART-")
+            if line == "":
+                if len(token_list) > 0:
+                    assert len(token_list) == len(label_list), "Length of tokens and labels have to be the same."
+                    inputs.append(token_list)
+                    labels.append(label_list)
+                token_list, label_list = [], []
+                continue
+
+            splits = line.split(DELIMITER_CONLL)  # CONLL format with just two non-empty columns
+            token = splits[0]
+            label = splits[1]
+
+            token_list.append(token)
+            label_list.append(label)
+        inputs.append(token_list)
+        labels.append(label_list)
+    return list(zip(inputs, labels))
+
+
+def _split_data(data: list[Tuple], train_ratio=0.8, test_ratio=0.1) -> dict[str, list]:
+    """Splits the data randomly into train, valid, and test sets."""
+    assert len(data) >= 3, "Data must have at least 3 samples to split into train, valid, and test sets."
+    random.shuffle(data)
+
+    train_size = max(1, int(len(data) * train_ratio))
+    test_size = max(1, int(len(data) * test_ratio))
+    valid_size = len(data) - train_size - test_size
+
+    first_idx = train_size
+    second_idx = train_size + valid_size
+    train_data = data[:first_idx]
+    valid_data = data[first_idx:second_idx]
+    test_data = data[second_idx:]
+
+    assert len(train_data) > 0, "Train set must have at least 1 sample."
+    assert len(test_data) > 0, "Test set must have at least 1 sample."
+    assert len(valid_data) > 0, "Valid set must have at least 1 sample."
+
+    print(f"Split {len(data)} items: (Train: {len(train_data)}, Valid: {len(valid_data)}, Test: {len(test_data)})\n")
+    return {"train": train_data, "valid": valid_data, "test": test_data}
+
+
+def _write_data(filename, data):
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(f"{DELIMITER_TSV.join(COLUMNS)}\n")
+        for item in data:
+            words, labels = item
+            assert len(words) == len(labels), "Length of tokens and labels have to be the same."
+
+            for word, label in zip(words, labels):
+                if word == '"':
+                    word = '\\"'  # escape double quotes
+                file.write(f"{word}{DELIMITER_TSV}{label}\n")
+            file.write("\n")
+
+
+def shuffle_and_split(conll_file):
+    file_content = _read_data(conll_file)
+    datasets = _split_data(file_content, train_ratio=0.8, test_ratio=0.1)
+
+    for file in datasets.keys():
+        # check if files exist
+        if Path(f"{file}.tsv").exists():
+            raise FileExistsError(f"File {file} already exists. Please remove all .tsv files and try again.")
+
+    for key, data in datasets.items():
+        print(f"{key}: {len(data)}")
+        _write_data(key + ".tsv", data)
+
+
+if __name__ == "__main__":
+    shuffle_and_split("computerscreens2023_labeled.conll")
+    print("Data split and saved three .tsv files in the current directory.")
--- tests/token_classification/test_dataset_cs2023.py
+++ tests/token_classification/test_dataset_cs2023.py
@@ -1,28 +1,57 @@
 from pathlib import Path
 
-from datasets import load_from_disk
+import pytest
+from datasets import load_dataset
+from datasets import load_dataset_builder
 from transformers import BertTokenizer
 
-from ner_data.computerscreens2023.computerscreens2023 import ComputerScreen2023Dataset
+from ner_data.computerscreens2023.shuffle_and_split import _split_data
 
 DATASETS_PATH = Path(__file__).parent.parent.parent / "ner_data"
 TOKENIZER = BertTokenizer.from_pretrained("bert-base-uncased")
 
 
-def test_computer_screens_2023_dataset():
-    dataset = ComputerScreen2023Dataset(
-        DATASETS_PATH / "computerscreens2023" / "computerscreens2023_labeled.conll", TOKENIZER, max_seq_length=128
-    )
+def test_shuffle_and_split():
+    item_count = 10
+    data = [([f"sent_{i}word1", f"sent_{i}word2"], [f"label{i}_1", f"label{i}_2"]) for i in range(item_count)]
 
-    assert dataset
-    # assert dataset.__getitem__(0)
+    res = _split_data(data, train_ratio=0.8, test_ratio=0.1)
+
+    assert res
+    assert len(res["train"]) == 8
+    assert len(res["valid"]) == 1
+    assert len(res["test"]) == 1
+
+    # check all items unique and no duplication
+    # check items from train are not in valid or test
+    assert all(item not in res["valid"] for item in res["train"])
+    assert all(item not in res["test"] for item in res["train"])
+
+    # check items from valid are not in train or test
+    assert all(item not in res["train"] for item in res["valid"])
+    assert all(item not in res["test"] for item in res["valid"])
+
+    # check items from test are not in train or valid
+    assert all(item not in res["train"] for item in res["test"])
+    assert all(item not in res["valid"] for item in res["test"])
 
 
-def test_load_dataset():
-    train_dataset = ComputerScreen2023Dataset(
-        DATASETS_PATH / "computerscreens2023" / "train.txt", TOKENIZER, max_seq_length=128
-    )
+def test_custom_dataset():
+    data_files = {
+        "train": str(DATASETS_PATH / "computerscreens2023" / "train.tsv"),
+        "test": str(DATASETS_PATH / "computerscreens2023" / "test.tsv"),
+        "valid": str(DATASETS_PATH / "computerscreens2023" / "valid.tsv"),
+    }
+    local_csv_dataset = load_dataset("csv", data_files=data_files, sep="\t")
 
-    train = load_from_disk(train_dataset)
+    for dataset in local_csv_dataset.values():
+        assert dataset.column_names == ["tokens", "ner_tags"]
 
-    assert train
+
+@pytest.mark.skip(reason="This test is not yet implemented.")
+def test_custom_dataset_loader():
+    script_path = str(DATASETS_PATH / "computerscreens2023")
+    local_csv_dataset = load_dataset_builder(script_path)
+
+    for dataset in local_csv_dataset.values():
+        assert dataset.column_names == ["token", "ner_tags"]
