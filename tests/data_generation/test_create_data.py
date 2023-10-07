d4d276cd42ece4772592cc01bff447e623550ec4
MattHag <matthias.hagmann@gmx.at>
2023-10-07 15:16:00 +0200

    Update manual monitor listing test
    Relates #3

--- tests/data_generation/test_create_data.py
+++ tests/data_generation/test_create_data.py
@@ -5,9 +5,10 @@ from data_generation.browser import Browser
 
 
 @pytest.mark.skip(reason="Only for manual tests, not for CI")
-def test_scrape_data_from_web():
+def test_get_monitor_listing():
     """This test actually scrapes data from the web."""
     max_products = 3
     with Browser(headless=False) as browser:
         products = create_data.retrieve_all_products(browser, max_products=max_products)
-        create_data.retrieve_product_details(browser, products)
+
+    assert len(products) == max_products
