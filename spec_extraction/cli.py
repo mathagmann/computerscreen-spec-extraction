a4ec3597acb7be0bb2f00b76151c68d8085e2169
MattHag <matthias.hagmann@gmx.at>
2023-10-26 04:14:24 +0200

    Adapt main according to changes
    Relates #22, #31

--- spec_extraction/cli.py
+++ spec_extraction/cli.py
@@ -1,5 +1,7 @@
 """CLI interface for specification_extraction project.
 """
+from pathlib import Path
+
 from spec_extraction.extraction_config import MonitorParser
 from spec_extraction.process import CATALOG_EXAMPLE
 from spec_extraction.process import Processing
@@ -7,6 +9,13 @@ from spec_extraction.process import Processing
 
 def main():  # pragma: no cover
     print("This will do something")
-    p = Processing(MonitorParser(), "interim", "processed")
+
+    base_path = Path("/Users/matthias/PycharmProjects/specification-extraction")
+
+    data_dir = base_path / "data_10"
+    output_dir_specs =  base_path / "out_10"
+    output_dir_catalog = base_path / "out_10_catalog"
+
+    p = Processing(MonitorParser(), data_dir, output_dir_specs)
     p.find_mappings(CATALOG_EXAMPLE)
-    p.create_monitor_specs()
+    p.create_monitor_specs(output_dir_catalog)
