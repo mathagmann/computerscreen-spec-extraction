aaab9e7c2dfc43e4c66970af9f77bfc9b06c81a3
MattHag <matthias.hagmann@gmx.at>
2023-11-17 20:11:54 +0100

    Simplify global path management
    Introduce config module to hold global data shared between
    multiple packages.
    
    Relates #49

---
+++ config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+DATASET_NAME = "computerscreens2023"  # Switch between datasets here
+
+ROOT_DIR = Path(__file__).parent
+LOG_DIR = ROOT_DIR / "logs"
+TEST_DIR = ROOT_DIR / "tests"
+
+ROOT_DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = ROOT_DATA_DIR / DATASET_NAME
+
+PRODUCT_CATALOG_DIR = ROOT_DATA_DIR / f"{DATASET_NAME}_product_catalog"
+RAW_SPECIFICATIONS_DIR = ROOT_DATA_DIR / f"{DATASET_NAME}_raw_specs"
+RAW_SPECIFICATIONS_TEXT_DIR = ROOT_DATA_DIR / f"{DATASET_NAME}_token_labeling"
