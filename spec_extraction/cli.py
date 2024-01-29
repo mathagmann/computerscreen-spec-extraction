71540496531079ac17d512df9e89914592a64ba3
MattHag <matthias.hagmann@gmx.at>
2024-01-29 11:13:15 +0100

    Update CLI according to previous refactorings

--- spec_extraction/cli.py
+++ spec_extraction/cli.py
@@ -1,14 +1,16 @@
 """CLI interface for specification_extraction project.
 """
 
+from config import DATA_DIR
 from config import PRODUCT_CATALOG_DIR
 from spec_extraction.bootstrap import bootstrap
 from spec_extraction.catalog_model import CATALOG_EXAMPLE
+from spec_extraction.process import extract_raw_specifications
 
 
 def main():  # pragma: no cover
-    processing = bootstrap()
-    processing.extract_raw_specifications()
+    extract_raw_specifications(DATA_DIR)
 
+    processing = bootstrap()
     processing.find_mappings(CATALOG_EXAMPLE)
     processing.merge_monitor_specs(PRODUCT_CATALOG_DIR)
