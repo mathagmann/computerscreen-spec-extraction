add117733ce2f45da38187653b6a3f88e0815bfe
MattHag <16444067+MattHag@users.noreply.github.com>
2024-03-24 15:12:40 +0100

    Evaluation: Show scores per attribute too
    Precision, Recall and F1 scores go down to property level, showing fine
    grained data for e.g.b Abmessungen, Gewicht etc.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from config import ROOT_DIR
 from spec_extraction.bootstrap import bootstrap as extraction_bootstrap
 from spec_extraction.evaluation.evaluate import evaluate_pipeline
 from spec_extraction.evaluation.evaluate import measure_time
+from spec_extraction.evaluation.evaluate import print_confusion_matrix_per_attr
 
 DEFAULT_FIELD_MAPPINGS = ROOT_DIR / "spec_extraction" / "preparation" / "field_mappings.json"
 
@@ -14,18 +15,18 @@ def evaluate_base_pipeline():
     processing_instance = extraction_bootstrap(field_mappings=DEFAULT_FIELD_MAPPINGS, machine_learning_enabled=False)
     processing_instance.merge_monitor_specs(PRODUCT_CATALOG_DIR)
 
-    confusion_matrix, product_precision = evaluate_pipeline(processing_instance)
+    confusion_matrix, cm_per_attr, product_precision = evaluate_pipeline(processing_instance)
 
-    return confusion_matrix, product_precision
+    return confusion_matrix, cm_per_attr, product_precision
 
 
 @measure_time
 def evaluate_base_pipeline_with_machine_learning():
     processing_instance = extraction_bootstrap(field_mappings=DEFAULT_FIELD_MAPPINGS, machine_learning_enabled=True)
 
-    confusion_matrix, product_precision = evaluate_pipeline(processing_instance)
+    confusion_matrix, cm_per_attr, product_precision = evaluate_pipeline(processing_instance)
 
-    return confusion_matrix, product_precision
+    return confusion_matrix, cm_per_attr, product_precision
 
 
 @measure_time
@@ -34,21 +35,36 @@ def evaluate_base_pipeline_with_manual_mapping():
     processing_instance = extraction_bootstrap(field_mappings=enhanced_field_mappings, machine_learning_enabled=False)
     processing_instance.merge_monitor_specs(PRODUCT_CATALOG_DIR)
 
-    confusion_matrix, product_precision = evaluate_pipeline(processing_instance)
+    confusion_matrix, cm_per_attr, product_precision = evaluate_pipeline(processing_instance)
 
-    return confusion_matrix, product_precision
+    return confusion_matrix, cm_per_attr, product_precision
 
 
 @measure_time
 def main():
-    scores_base, product_precision_base = evaluate_base_pipeline()
-    scores_machine_learning, product_precision_machine_learning = evaluate_base_pipeline_with_machine_learning()
-    scores_manual, product_precision_manual = evaluate_base_pipeline_with_manual_mapping()
+    scores_base, cm_per_attr_base, product_precision_base = evaluate_base_pipeline()
+    scores_machine_learning, cm_per_attr_bert, product_precision_bert = evaluate_base_pipeline_with_machine_learning()
+    scores_manual, cm_per_attr_manual, product_precision_manual = evaluate_base_pipeline_with_manual_mapping()
 
     logger.info(f"{'*'*10} Evaluation results {'*'*10}")
-    logger.info(f"Base pipeline:\nConfusion matrix{scores_base}\n{scores_base.eval_score}")
-    logger.info(f"Machine learning:\nConfusion matrix{scores_machine_learning}\n{scores_machine_learning.eval_score}")
-    logger.info(f"Manual mapping:\nConfusion matrix{scores_manual}\n{scores_manual.eval_score}")
+    logger.info(
+        f"{'#'*5} Base pipeline results {'#'*5}\n{scores_base}\n{scores_base.eval_score}\n"
+        f"Product precision: {product_precision_base:.2f}%"
+    )
+    print_confusion_matrix_per_attr(cm_per_attr_base)
+
+    logger.info(
+        f"{'#'*5} Machine learning results {'#'*5}\n{scores_machine_learning}\n"
+        f"{scores_machine_learning.eval_score}\n"
+        f"Product precision: {product_precision_bert:.2f}%"
+    )
+    print_confusion_matrix_per_attr(cm_per_attr_bert)
+
+    logger.info(
+        f"{'#'*5} Manual mapping results {'#'*5}\n{scores_manual}\n{scores_manual.eval_score}\n"
+        f"Product precision: {product_precision_manual:.2f}%"
+    )
+    print_confusion_matrix_per_attr(cm_per_attr_manual)
 
 
 if __name__ == "__main__":
